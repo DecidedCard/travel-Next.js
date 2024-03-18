c8f5793fca328691367afa476f8afed2da83904b
DecidedCard <heajun35@gmail.com>
2024-03-18 21:12:07 +0900

    [feat] react-quill next rendering 오류 해결

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -3,6 +3,7 @@ import InputForm from "@/components/writeComponents/InputForm";
 const Write = () => {
   return (
     <div className="min-w-[1280px] max-w-7xl mx-auto">
+      <h2>글 작성 페이지</h2>
       <InputForm />
     </div>
   );
--- src/hook/useWriteInputForm.ts
+++ src/hook/useWriteInputForm.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import useInput from "./useInput";
 
 const useWriteInputForm = () => {
