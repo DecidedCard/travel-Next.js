e8bb12e65cf96aa47947a8f6d0e931ef223039bd
C1oudys <hymin310@gmail.com>
2024-03-18 21:57:51 +0900

    feat: 검색칸 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,22 @@ import {
   CardBody,
   Image as NextUiImg,
   User,
+  Input,
 } from "@nextui-org/react";
-import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 
 const Home: React.FC = () => {
   return (
     <div>
-      <Image src={mainImage} alt="Main" className="w-full h-[396px] block" />
-      <div className="mt-10 flex">
-        <Button color="primary" className="ml-5 mr-5">
-          최신 순
-        </Button>
-        <Button color="default">댓글 순</Button>
+      <div className="relative w-full h-[396px]">
+        <Image src={mainImage} alt="Main" layout="fill" objectFit="cover" />
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full flex justify-center">
+          <input
+            type="text"
+            placeholder="어느 나라, 도시로 떠나시나요?"
+            className="w-3/4 md:w-1/2 lg:w-1/3 h-10 px-4 py-2 bg-white bg-opacity-90 rounded-full shadow outline-none ring-2 ring-blue-500 focus:bg-opacity-100"
+          />
+        </div>
       </div>
       <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 p-5">
         <Card className="py-4">
