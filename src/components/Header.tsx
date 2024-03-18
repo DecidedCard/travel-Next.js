ed330e6ad755f1631350b14da706994a73bc5702
C1oudys <hymin310@gmail.com>
2024-03-18 22:57:03 +0900

    feat: header logo, home link 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "../../public/logo.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <nav className="bg-blue-500 text-white px-4 py-2 flex justify-between items-center">
       <div className="flex items-center space-x-4">
-        <Image src={logo} alt="Logo" width={40} height={40}/> 
+      <Link href="/">
+            <Image src={logo} alt="Logo" width={40} height={40}/>
+        </Link>
         <div className="flex flex-col">
           <h1 className="font-bold text-xl">여행한탕</h1>
           <span className="text-sm font-light">good travel && good experience</span>
