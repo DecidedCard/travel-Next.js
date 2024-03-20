2ee5e98e8f91a36830fc6b61f738192411e56fc3
DecidedCard <heajun35@gmail.com>
2024-03-20 09:37:34 +0900

    [feat] 디테일 틀 작성

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,8 +1,13 @@
+import PostDetail from "@/components/detailComponents/PostDetail";
 import React from "react";
 
-const Detail = ({ params }: { params: { id: string } }) => {
-  console.log(params.id);
-  return <div>Detail</div>;
+const Detail = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  return (
+    <div>
+      <PostDetail id={id} />
+    </div>
+  );
 };
 
 export default Detail;
