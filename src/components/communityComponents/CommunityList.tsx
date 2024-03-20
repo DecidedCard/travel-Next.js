5b8dda6761f46ebb3556a29f9aed6c53ba6c31b8
KY <rudyeon@naver.com>
2024-03-20 12:33:39 +0900

    [feat] community 목록 조회

--- src/components/communityComponents/CommunityList.tsx
+++ src/components/communityComponents/CommunityList.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+import useCommunityStore from "@/store/communityStore";
+import React, { useEffect } from "react";
 
 const CommunityList = () => {
-  return <div>CommunityList</div>;
+  const { communityContent, fetchCommunity } = useCommunityStore();
+
+  useEffect(() => {
+    fetchCommunity();
+  }, []);
+
+  return (
+    <div>
+      <h2>Community List</h2>
+      <ul>
+        {communityContent.map((post) => (
+          <li key={post.id}>
+            <p>{post.communityContent}</p>
+            <p>작성자: 임시test123</p>
+            <p>작성일: {new Date(post.created_at).toLocaleString()}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CommunityList;
