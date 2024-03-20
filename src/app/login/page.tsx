7e87bf19aa605ac59f2e04b0d7093390a3b163b3
seoyeon-1206 <ssy_1206@naver.com>
2024-03-20 15:49:45 +0900

    [style] 로그인 페이지 input nextUI 파일 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,12 +5,18 @@ import useAuthStore from "@/store/authStore";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import loginImg from "@/assets/loginImg.jpg";
+import { Button, Card, Input } from "@nextui-org/react";
+import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
+import EyeFilledIcon from "./EyeFilledIcon";
 
 const Login = () => {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { authLogin } = useAuthStore();
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -26,30 +32,47 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <img
-        src={loginImg.src}
-        alt="LoginBackgroundImg"
-        className="absolute inset-0 w-full h-full object-cover"
-      />
-      <div className="bg-cover bg-center w-full h-full">
-        <form onSubmit={handleLogin}>
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button type="submit">로그인</button>
-        </form>
+    <div className="relative">
+      <div>
+        <img src={loginImg.src} alt="LoginBackgroundImg" className="absolute" />
+      </div>
+      <div className="absolute">
+        <Card className="py-20 w-[560px] h-[682px]">
+          <form onSubmit={handleLogin}>
+            <div className="flex flex-col gap-4">
+              <Input
+                type="email"
+                label="Email"
+                variant="flat"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                label="Password"
+                variant="flat"
+                endContent={
+                  <button
+                    className="focus:outline-none"
+                    type="button"
+                    onClick={toggleVisibility}
+                  >
+                    {isVisible ? (
+                      <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    ) : (
+                      <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    )}
+                  </button>
+                }
+                type={isVisible ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <Button type="submit" color="primary">
+              로그인
+            </Button>
+          </form>
+        </Card>
       </div>
     </div>
   );
