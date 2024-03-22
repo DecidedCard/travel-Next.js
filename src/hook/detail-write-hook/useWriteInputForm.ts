b2a96b8ee8ccfe330db0a106ba008a01d1ad4ab0
C1oudys <hymin310@gmail.com>
2024-03-22 21:15:31 +0900

    merge

--- src/hook/detail-write-hook/useWriteInputForm.ts
+++ src/hook/detail-write-hook/useWriteInputForm.ts
@@ -94,7 +94,7 @@ const useWriteInputForm = (post?: Post) => {
         travelDate: `${startDate} ~ ${endDate}`,
         travelPlace,
         postMainContent,
-        postBasicImage,
+        postBasicImage
       };
       updateMutate({ id, post });
       location.reload();
