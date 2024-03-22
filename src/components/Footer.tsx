02667e8283afc5076a87e7c21c02c7e0d7d66e6e
C1oudys <hymin310@gmail.com>
2024-03-22 16:19:58 +0900

    feat: 임시 Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { FaGithub } from 'react-icons/fa';
+import { RiNotionFill } from "react-icons/ri";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-gray-800 text-white text-center p-5 mt-3">
+      <p className="text-xl font-bold text-gray-300 mb-2">여행한탕</p>
+      <p className="max-w-md mx-auto mb-2">Your Trip will be more Memorable with us.</p>
+      <div className="flex justify-center space-x-6 my-1">
+        <a href="https://github.com/DecidedCard/travel-Next.js" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-white transition-colors duration-300">
+          <FaGithub size="28" />
+        </a>
+        <a href="https://teamsparta.notion.site/8-e2eb6b4d004b406397903885d2444934" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-white transition-colors duration-300">
+          <RiNotionFill size="28" />
+        </a>
+      </div>
+      <div>
+        <p className="text-gray-500 text-sm">Copyright © 2024 여행한탕. All rights reserved. Designed By 여행한탕</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
